93ad6fde91bb648c66539501e9dc3534a91a4883
Tin Trinh <trinh.tin@hamee.co.jp>
2020-07-19 23:29:46 +0900

    Add nebular style

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -5,23 +5,65 @@ const BoardSize = 3;
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
-  public boardValues : String[][];
+  public boardValues: String[];
+  public oIsNext: Boolean;
+  public winner: String;
 
+  constructor() {}
 
-  constructor() {
-    this.boardValues = [];
-    for(let i = 0; i < BoardSize; i++) {
-      this.boardValues[i] = [];
-      for (let j = 0; j < BoardSize; j++) {
-        this.boardValues[i].push(Math.random() > 0.5 ? 'X' : 'O');
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.boardValues = Array(9).fill('');
+    this.oIsNext = false;
+    this.winner = null;
+  }
+
+  get player() {
+    return this.oIsNext ? 'O' : 'X';
+  }
+
+  makeMove(position: number) {
+    if (this.boardValues[position] === '') {
+      this.boardValues[position] = this.player;
+      this.oIsNext = !this.oIsNext;
+      let winner = this.calculateWinner(this.boardValues);
+
+      if (winner !== false) {
+        this.winner = winner;
       }
     }
   }
 
-  ngOnInit(): void {
-  }
+  calculateWinner(boardValues: String[]) {
+    let winLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
+    for (let line of winLines) {
+      let [i, j, k] = line;
+
+      if (
+        this.boardValues[i] !== '' &&
+        this.boardValues[i] == this.boardValues[j] &&
+        this.boardValues[j] === this.boardValues[k]
+      ) {
+        return this.boardValues[i];
+      }
+    }
+
+    return false;
+  }
 }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -5,11 +5,20 @@ type SquareValue = 'X' | 'O';
 @Component({
   selector: 'app-square',
   template: `
-    <span>
-      {{ val }}
-    </span>
+    <button nbButton *ngIf="!val"> {{ val }} </button>
+    <button nbButton hero status='success' *ngIf="val === 'X'"> {{ val }} </button>
+    <button nbButton hero status='info' *ngIf="val === 'O'"> {{ val }} </button>
   `,
-  styles: [],
+  styles: [
+    `
+    button {
+      font-size: 50px !important;
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+    `
+  ],
 })
 export class SquareComponent implements OnInit {
   @Input() val: SquareValue;
